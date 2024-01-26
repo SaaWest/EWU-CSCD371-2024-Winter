a9d7f67b958ac2c293bce79edf54650f707f8e16
SaaWest <swest6@ewu.edu>
2024-01-25 21:00:54 -0800

    made another edit

--- Logger.Tests/FileLoggerTestsBase.cs
+++ Logger.Tests/FileLoggerTestsBase.cs
@@ -9,7 +9,7 @@ namespace Logger.Tests;
     public FileLogger Logger { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     [TestInitialize]
-    public virtual void Log_FileLogger()
+    public virtual void LogFileLogger()
     {
         FilePath = Path.GetTempFileName();
         Logger = new FileLogger(nameof(FileLoggerTestsBase), FilePath);
