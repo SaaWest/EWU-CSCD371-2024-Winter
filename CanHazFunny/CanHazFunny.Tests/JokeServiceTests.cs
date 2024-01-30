d4440279f1f4728739d10ee99040171797cd131a
SaaWest <swest6@ewu.edu>
2024-01-29 17:50:14 -0800

    made tests

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CanHazFunny.Tests
 {
     internal class JokeServiceTests
     {
+        [Fact]
+        public void GtJoke_ReturnJoke()
+        {
+            JokeService jokeService = new JokeService();
+            string joke = jokeService.GetJoke();
+            Assert.IsNotNull(joke);
+
+        }
     }
 }
