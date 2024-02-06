653aa11443730bddac01430d131079774310afb1
SaaWest <swest6@ewu.edu>
2024-02-06 13:59:46 -0800

    created employee test needs work

---
+++ Logger.Tests/EmployeeRecordTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Tests;
+
+public class EmployeeRecordTests
+{
+    public void EmployeeRecord_Name_NotNull()
+    {
+        EmployeeRecord record = new EmployeeRecord();
+    }
+        
+}
+
