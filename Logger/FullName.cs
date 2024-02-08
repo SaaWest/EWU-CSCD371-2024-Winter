b14537b59cd411d3ec5c38895563b1ce54191631
SaaWest <swest6@ewu.edu>
2024-02-08 13:34:47 -0800

    changes to FullName

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -7,6 +7,7 @@ public record FullName
     //these three/two requirements is what is need when instantiate a new person
     //Records are immutable and so FullName should reflect that as first, middle, and last names do
     //not change in real life unless some legal entity supercedes that.
+    //FullName is a reference type since the object itself is refering to the data its pointing to
     public FullName(string firstName, string lastName, string? middleName = null)
     {
         FirstName = string.IsNullOrEmpty(firstName) ? throw new ArgumentNullException(nameof(firstName)) : firstName;
