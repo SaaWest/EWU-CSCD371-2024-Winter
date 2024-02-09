8a6023f7da413a343bdec4d22c9d460f83a70d87
Matthew-Powers <mpowers16@ewu.edu>
2024-02-08 22:10:58 -0800

    Garbage, remove

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -11,33 +11,33 @@ namespace Logger.Tests;
 
 public class FullNameTests
 {
-        [TestClass]
-        public class FullName_Record_Tests()
+    [TestClass]
+    public class FullName_Record_Tests()
+    {
+        [TestMethod]
+        [DataRow("Mike", "Rowe", "Soft")]
+        [DataRow("Michael", "B", "Jordon")]
+        public void FullName_Record_NotNull(string firstName, string middleName, string lastName)
         {
-            [TestMethod]
-            [DataRow("Mike", "Rowe", "Soft")]
-            //[DataRow("Michael", "B", "Jordon")]
-            public void FullName_Record_NotNull(string firstName, string middleName, string lastName)
-            {
-                FullName fullName = new FullName(firstName, middleName, lastName);
-              
-                Assert.Equals(fullName.FirstName, firstName);
-                Assert.Equals(fullName.MiddleName, middleName);
-                Assert.Equals(fullName.FirstName, lastName);
+            FullName fullName = new(firstName, middleName, lastName);
+
+            Assert.Equals(fullName.FirstName, firstName);
+            Assert.Equals(fullName.MiddleName, middleName);
+            Assert.Equals(fullName.FirstName, lastName);
 
 
-            }
-            [TestMethod]
-            [DataRow("Micheal", null, "Jordon")]
-            public void FullName_Record_WithNull(string firstName, string middleName, string lastName)
-            {
-                FullName fullName = new FullName(firstName, middleName, lastName);
-
-                Assert.Equals(fullName.FirstName, firstName);
-                Assert.Equals(fullName.MiddleName, middleName);
-                Assert.Equals(fullName.FirstName, lastName);
-
-            }
         }
+        [TestMethod]
+        [DataRow("Micheal", null, "Jordon")]
+        public void FullName_Record_WithNull(string firstName, string middleName, string lastName)
+        {
+            FullName fullName = new(firstName, middleName, lastName);
+
+            Assert.Equals(fullName.FirstName, firstName);
+            Assert.Equals(fullName.MiddleName, middleName);
+            Assert.Equals(fullName.FirstName, lastName);
+
+        }
+    }
 
 }
