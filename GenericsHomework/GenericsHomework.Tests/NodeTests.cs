4495cf2a6a8107c43f2ca6bec567948e3a5b7b73
SaaWest <swest6@ewu.edu>
2024-02-09 13:31:26 -0800

    added tests and worked on node class

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace GenericsHomework.Tests;
+public class NodeTests
+{
+    [Fact]
+    public void NodeTests_FirstNode_ValueNotNull_NullPrev_NullNext()
+    {
+        var myNode = new Node(12, null!, null!);
+
+        Assert.NotNull(myNode.Value);
+        //Assert.Null(myNode.Next);
+        Assert.Null(myNode.Prev);
+    }
+    [Fact]
+    public void NodeTests_FirstNode_ValueNotNull_Prev_Next()
+    {
+        var myNode = new Node(4, null!, null!);
+        var myNode2 = new Node(12, myNode, null!);
+        myNode.Next = myNode2;
+        Assert.NotNull(myNode.Value);
+        Assert.Null(myNode.Prev);
+        //Assert.NotNull(myNode.Next);
+        Assert.NotNull(myNode2.Prev);
+    }
+    [Fact]
+    public void ToString_Override_Tests()
+    {
+        var myNode = new Node(12, null!, null!);
+        Console.WriteLine(myNode);
+
+    }
+}
+
---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework;
+public class Node
+{
+    public Node _Next;
+    public Node(object value, Node prev, Node next)
+    {
+        Value = value; Prev = prev;_Next = next;
+    }   
+    public object Value { get; }
+    public Node? Prev { get; set; }
+    public Node Next
+    {
+        get
+        {
+            if (_Next == null)
+            {
+               return _Next = this;
+            }
+        }
+            private set;
+        
+    }
+
+    public override string ToString()
+    {
+        return "Value :" + Value+ "PrevNode :" + Prev +"NextNode : " + Next; 
+    }
+}
+
