b0666a5c0ab5b55380b0eda0ffdb60fc77ca9ead
SaaWest <swest6@ewu.edu>
2024-02-11 20:38:41 -0800

    edited some stuff to class

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -8,28 +8,38 @@ namespace GenericsHomework;
 public class Node
 {
     public Node _Next;
-    public Node(object value, Node prev, Node next)
+    public Node _Previous;
+    public Node(object value)
     {
-        Value = value; Prev = prev;_Next = next;
+        Value = value;
+        Prev = _Previous;
+        Next = _Next!;
     }   
     public object Value { get; }
     public Node? Prev { get; set; }
     public Node Next
     {
-        get
-        {
-            if (_Next == null)
-            {
-               return _Next = this;
-            }
-        }
-            private set;
+        get{ return _Next!;}
+        private set { _Next = value; }
         
     }
 
     public override string ToString()
     {
-        return "Value :" + Value+ "PrevNode :" + Prev +"NextNode : " + Next; 
+        return "Value :" + Value.ToString(); 
+    }
+    public void Append(object value)
+    {
+        if (Next != this) 
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Next;
+            Next = newNode;
+        }
+        else
+        {
+            Next = new Node(value);
+        }
     }
 }
 
