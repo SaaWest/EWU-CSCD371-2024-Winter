6fb439b9f7413ea9eecc6bf0077ca494b7caea7d
Matthew-Powers <mpowers16@ewu.edu>
2024-02-13 16:04:29 -0800

    Reformatted tests to only test an individual component

--- GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework.Tests/NodeTests.cs
@@ -2,23 +2,42 @@
 public class NodeTests
 {
     [Fact]
-    public void NodeTests_NodeNotNullAndNextNotNull_Sucess()
+    public void NodeTests_NodeNotNull_Sucess()
     {
         Node<string> myNode = new("12");
 
         Assert.NotNull(myNode.Value);
+    }
+
+    [Fact]
+    public void NodeTests_NodeNextNotNull_Success()
+    {
+        Node<string> myNode = new("12");
+
         Assert.NotNull(myNode.Next);
     }
 
     [Fact]
-    public void NodeTests_AddMultipleUniqueNodes_Success()
+    public void NodeTests_AddMultipleUniqueNodesNodeNotNull_Success()
     {
         Node<string> myNode = new("4");
         
         myNode.Append("12");
         myNode.Append("23");
         myNode.Append("18");
+
         Assert.NotNull(myNode.Value);
+    }
+
+    [Fact]
+    public void NodeTests_AddMultipleUniqueNodesNextValueNotNull_Success()
+    {
+        Node<string> myNode = new("9");
+
+        myNode.Append("1");
+        myNode.Append("2");
+        myNode.Append("8");
+
         Assert.NotNull(myNode.Next.Value);
     }
 
@@ -28,9 +47,18 @@ public class NodeTests
         Node<string> myNode = new("4");
 
         myNode.Append("8");
-        Assert.NotNull(myNode.Value);
-        Assert.NotNull(myNode.Next);
 
+        Assert.NotNull(myNode.Value);
+    }
+
+    [Fact]
+    public void NodeTests_FirstAndSecondNodeUniqueNextNotNull_Success()
+    {
+        Node<string> myNode = new("45");
+
+        myNode.Append("98");
+
+        Assert.NotNull(myNode.Next);
     }
 
     [Fact]
@@ -47,8 +75,8 @@ public class NodeTests
         Node<string> myNode = new("12");
         
         myNode.Append("4");
-        Assert.NotNull(myNode.Next);
         myNode.Clear();
+
         Assert.Equal("12", myNode.Value);
     }
 
