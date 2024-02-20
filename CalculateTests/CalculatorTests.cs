e6689de549e8adb9c0b15c2bae0b39ae92a0dae8
SaaWest <swest6@ewu.edu>
2024-02-20 13:17:45 -0800

    made changes

--- CalculateTests/CalculatorTests.cs
+++ CalculateTests/CalculatorTests.cs
@@ -1,9 +1,6 @@
-﻿using Calculate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Xunit;
+using Calculate;
 
 namespace CalculateTests;
 
