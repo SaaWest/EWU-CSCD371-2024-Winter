270d3ec3ea410d245c68562bf9d67ec77b810dc0
Matthew-Powers <mpowers16@ewu.edu>
2024-02-20 14:23:20 -0800

    More test name rewrites

--- CalculateTests/ProgramTests.cs
+++ CalculateTests/ProgramTests.cs
@@ -4,7 +4,7 @@ namespace CalculateTests;
 public class ProgramTests
 {
     [Fact]
-    public void Program_ReadWrite_ValidTests()
+    public void Program_ReadWrite_ValidTestSuccess()
     {
         string line= "Can you Write this?";
         string output = "";
@@ -14,7 +14,7 @@ public class ProgramTests
         Assert.Equal(line, output);
     }
     [Fact]
-    public void Program_ReadWrite_InvalidTests()
+    public void Program_ReadWrite_InvalidTestFailure()
     {
         string line = "Can you Write this?";
         string output = "";
