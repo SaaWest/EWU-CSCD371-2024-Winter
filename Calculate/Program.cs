6712f6948182393f8f860f442f7b1fc7a9843bd5
SaaWest <swest6@ewu.edu>
2024-02-20 13:20:10 -0800

    fixed namespace

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,31 +1,30 @@
 ﻿using Calculate;
 
-namespace CalculateTests
+namespace Calculate;
+
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
+        Program program = new Program();
+        double response;
+        string input;
+        do
         {
-            Program program = new Program();
-            double response;
-            string input;
-            do
-            {
-                program.Writer("Enter your Math expression");
-                input = program.Reader()!;
+            program.Writer("Enter your Math expression");
+            input = program.Reader()!;
 
 
-            } while (!Calculator.TryCalculate(input, out response));
-            program.Writer($"Answer: {response}");
+        } while (!Calculator.TryCalculate(input, out response));
+        program.Writer($"Answer: {response}");
 
-        }
-        public Program()
-        {
-
-
-        }
-
-        public Func<string?> Reader { get;  init; } = Console.ReadLine;
-        public Action<string> Writer { get; init; } = Console.WriteLine;
     }
+    public Program()
+    {
+
+
+    }
+
+    public Func<string?> Reader { get;  init; } = Console.ReadLine;
+    public Action<string> Writer { get; init; } = Console.WriteLine;
 }
